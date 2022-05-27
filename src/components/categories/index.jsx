81bc77f188956e6fb19c0ddfa169d971e6876eb1
gapur <kaloycant@gmail.com>
2022-05-27 19:08:02 +0300

    render categories

--- src/components/categories/index.jsx
+++ src/components/categories/index.jsx
@@ -1,13 +1,28 @@
+import React from 'react';
+
+const categories = ["Все", "Мясные", "Вегетарианская", "Гриль", "Острые", "Закрытые"];
+
 const Categories = () => {
+  const [activeIndex, setActiveIndex] = React.useState(0);
+
+  const setActiveCategory = (index) => {
+    setActiveIndex(index);
+  };
+
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
+        {
+          categories.map((item, index) => {
+            return <li key={item} onClick={() => setActiveCategory(index)} className={activeIndex === index ? "active" : ""}>{item}</li>
+          })
+        }
+        {/* <li className="active">Все</li>
         <li>Мясные</li>
         <li>Вегетарианская</li>
         <li>Гриль</li>
         <li>Острые</li>
-        <li>Закрытые</li>
+        <li>Закрытые</li> */}
       </ul>
     </div>
   );
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,9 +1,11 @@
+import pizzaLogo from "../../assets/img/pizza-logo.svg";
+
 const Header = () => {
   return (
     <div className="header">
       <div className="container">
         <div className="header__logo">
-          <img width="38" src="./img/pizza-logo.svg" alt="Pizza logo" />
+          <img width="38" src={pizzaLogo} alt="Pizza logo" />
           <div>
             <h1>React Pizza</h1>
             <p>самая вкусная пицца во вселенной</p>

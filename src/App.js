6f598424416e115f15bf5c3d4787cad3f1362476
gapur <kaloycant@gmail.com>
2022-05-27 17:51:59 +0300

    create first components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/header";
+import Categories from "./components/categories";
+import Sort from "./components/sort";
+import PizzaBlock from "./components/card";
+
+import "./scss/app.scss";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort /> 
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            <PizzaBlock title="Чизбургер-пицца" price="12" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }

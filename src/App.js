2f2c192deb481a442893f42fa743d6f037f51629
gapur <kaloycant@gmail.com>
2022-05-31 15:36:45 +0300

    add skeleton loader

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/header";
 import Categories from "./components/categories";
 import Sort from "./components/sort";
 import PizzaBlock from "./components/card";
+import Loader from "./components/card/skeleton";
 
 // import pizzas from "./assets/db/pizzas.json";
 
@@ -10,11 +12,15 @@ import "./scss/app.scss";
 
 function App() {
   const [items, setItems] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
 
   React.useEffect(() => {
     fetch("https://6293ec25089f87a57ac77f49.mockapi.io/items")
       .then((res) => res.json())
-      .then((arr) => setItems(arr));
+      .then((arr) => {
+        setItems(arr);
+        setIsLoading(false);
+      });
   }, []);
 
   return (
@@ -28,9 +34,13 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {items.map((obj) => {
-              return <PizzaBlock key={obj.id} {...obj} />;
-            })}
+            {isLoading
+              ? [...new Array(6)].map((_, index) => {
+                  return <Loader key={index} />;
+                })
+              : items.map((obj) => {
+                  return <PizzaBlock key={obj.id} {...obj} />;
+                })}
           </div>
         </div>
       </div>

8ea4925d6d79dfb3e2731460497b9071a264db04
gapur <kaloycant@gmail.com>
2022-06-09 19:21:30 +0300

    add listner for close sort

--- src/components/sort/index.jsx
+++ src/components/sort/index.jsx
@@ -13,6 +13,7 @@ const Sort = () => {
   
   const dispatch = useDispatch();
   const sort = useSelector(state => state.filter.sort )
+  const sortRef = React.useRef()
 
   const setActive = (obj) => {
     dispatch(setSort(obj));
@@ -23,8 +24,21 @@ const Sort = () => {
     dispatch(setSort({...sort, sortOrder: order}));
   };
 
+  React.useEffect(() => {
+    const handleClickOutside = (event) => {
+      if(!event.path.includes(sortRef.current)) {
+        setOpen(false);
+      }
+    };
+    document.body.addEventListener('click', handleClickOutside);
+
+    return () => {
+      document.body.addEventListener('click', handleClickOutside);
+    }
+  }, []);
+
   return (
-    <div className={`sort${open ? " active" : ""}`}>
+    <div ref={sortRef} className={`sort${open ? " active" : ""}`}>
       <div className="sort__label">
         <svg
           width="10"

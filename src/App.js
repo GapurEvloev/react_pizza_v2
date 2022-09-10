bb270879a944ae8977f37650071ba632fdcba9a7
gapur <kaloycant@gmail.com>
2022-09-10 23:18:13 +0200

    #6 fin - get pizzas from BE

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
+import React from "react";
 import Header from "./components/Header";
 import Categories from "./components/Categories";
 import SortPopup from "./components/SortPopup";
 import PizzaBlock from "./components/PizzaBlock";
 
-import pizzasData from "./assets/data/pizzas.json";
-
 import "./scss/app.scss";
+import axios from "axios";
 
 function App() {
+  const [items, setItems] = React.useState([]);
+
+  React.useEffect(() => {
+    axios
+      .get("https://6293ec25089f87a57ac77f49.mockapi.io/items")
+      .then((res) => setItems(res.data))
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <div className="App">
       <div className="wrapper">
@@ -20,7 +31,7 @@ function App() {
             </div>
             <h2 className="content__title">Все пиццы</h2>
             <div className="content__items">
-              {pizzasData.map((pizza) => {
+              {items.map((pizza) => {
                 return <PizzaBlock key={pizza.id} {...pizza} />;
               })}
             </div>
